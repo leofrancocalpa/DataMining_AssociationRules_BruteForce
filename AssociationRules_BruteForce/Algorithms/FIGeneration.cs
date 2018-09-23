5f1ed410a18f196097dfd65a6f79568186a13245
leofrancocalpa <francocalpa@gmail.com>
2018-09-23 17:53:54 -0500

    AN001L

---
+++ AssociationRules_BruteForce/Algorithms/FIGeneration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class FIGeneration
+    {
+        //Constructor
+        public FIGeneration()
+        {
+            
+        }
+
+        //Frequent Intemset Generation Apriori Algorithm 
+        public void FrequentIGApriorio()
+        {
+            int k = 1; // size k of itemset (k-itemset)
+
+        }
+    }
+}
